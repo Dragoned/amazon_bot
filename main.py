8f0565b4e55e1e0563ee10120769223b1fff6fe3
lorenzo <lorenzoprotano22@gmail.com>
2021-11-11 15:46:17 +0100

    added log file exeption handler config for file serch query

--- main.py
+++ main.py
@@ -51,15 +51,21 @@
 # bs4 (Beautifull soup 4)
 # request
 
+from os import execlp
+import re
 import json
 import pickle
 import webbrowser
 import requests
+import threading
+import logging
 import matplotlib.pyplot as plt
-from pynput import keyboard
 
 from datetime import datetime
 from bs4 import BeautifulSoup
+
+logging.basicConfig(format='%(asctime)s %(levelname)s - %(funcName)s:%(message)s',
+                    filename='amazon_bot.log', encoding='utf-8', level=logging.DEBUG, datefmt='%m/%d/%Y %I:%M:%S %p')
 
 
 class amazonpy():
@@ -67,7 +73,6 @@ class amazonpy():
 
     # --settings--
     settings = json.load(open("./settings.json"))
-    # --settings--
 
     def __init__(self):
         # 1 path choise
@@ -86,7 +91,7 @@ class amazonpy():
                 if not new_header == "":
                     self.settings["header"]["User-Agent"] = new_header
                     changed = True
-                
+
                 new_prod_file = input("inserisci in nuovo file prodotti: ")
                 if not new_prod_file == "":
                     self.settings["header"]["productfile"] = new_prod_file
@@ -99,10 +104,12 @@ class amazonpy():
         while True:
             try:
                 # checks the user agent
-                page = requests.get("https://www.google.com", headers=self.settings["header"])
+                page = requests.get("https://www.google.com",
+                                    headers=self.settings["header"])
 
                 if not page.status_code == 200:
-                    print("connection error "+page.status_code+" with the sequent user agent "+self.settings["header"])
+                    print("connection error "+page.status_code +
+                          " with the sequent user agent "+self.settings["header"])
                     return
 
                 if not changed:
@@ -143,7 +150,8 @@ class amazonpy():
                 changed = True
             elif option == 2:
                 self.show_products()
-                self.remove_product(int(input("Select which number would you like to delete")) - 1)
+                self.remove_product(
+                    int(input("Select which number would you like to delete")) - 1)
                 changed = True
             elif option == 3:
                 # ToDo self.url_list[int(input("Select which number would you like to modify")) - 1] = input("Insert the link --> ")
@@ -162,7 +170,8 @@ class amazonpy():
                     print("Would you like save you changes? [y/n]")
                     save = input("-->")
                     if save == 'y':
-                        json.dump(self.product, open(self.settings["productfile"], "w"))
+                        json.dump(self.product, open(
+                            self.settings["productfile"], "w"))
                         return
                     elif save == 'n':
                         return
@@ -172,17 +181,6 @@ class amazonpy():
 
             else:
                 print("Error, invalid input")
-
-    def fetch(self):
-        if len(self.product) == 0:
-            print("You can't fetch if there's no data")
-            return
-
-        for i in range(len(self.product)):
-            try:
-                self.new_detection(i)
-            except:
-                print(f"Error, invalid link number  {i}  ")
 
     def compare(self):
         comp_file = open("products", "rb")
@@ -206,11 +204,13 @@ class amazonpy():
                 price = "".join(i for i in price if i != '€')
                 converted_price = float(price)
 
-                print(f"Product's n{x} (Before): {price_list[x]} ||| Product's price (Now) {converted_price}")
+                print(
+                    f"Product's n{x} (Before): {price_list[x]} ||| Product's price (Now) {converted_price}")
                 if (price_list[x] > converted_price):
                     print(
                         f"!!!Found discount in product's [{title.strip()}] differenze: {price_list[x] - converted_price} euros!!!")
-                    open_choice = input("Would you like to see the discounted product?  y | n \n--->")
+                    open_choice = input(
+                        "Would you like to see the discounted product?  y | n \n--->")
                     if (open_choice == 'y'):
                         webbrowser.open(self.url_list[x])
                     else:
@@ -219,83 +219,115 @@ class amazonpy():
                 print(f"Error could not find the price in link n{x}")
 
     def add_product(self, url):
-        page = requests.get(url, headers=self.settings["header"])
+        try:
+            page = requests.get(url, headers=self.settings["header"])
 
-        if not page.status_code == 200:
-            print("connection error "+page.status_code+" try later")
-            return
+        except requests.exceptions.HTTPError as errh:
+            logging.error("Http Error:", errh)
 
-        soup = BeautifulSoup(page.content, 'html.parser')
-        
-        new_product = { 
-            "productname":soup.find(id='productTitle').get_text().replace("\n", ""),
-            "url":url,
-            "tag": soup.find(class_="a-link-normal a-color-tertiary").get_text().replace("  ", "").replace('\n', ''),
-            "detectionprice":[]
+        except requests.exceptions.ConnectionError as errc:
+            logging.error("Error Connecting:", errc)
+
+        except requests.exceptions.Timeout as errt:
+            logging.error("Timeout Error:", errt)
+
+        except requests.exceptions.RequestException as err:
+            logging.error("OOps: Something Else", err)
+
+        else:
+            '''
+            if not page.status_code == 200:
+                print("connection error "+page.status_code+" try later")
+                return
+            '''
+
+            soup = BeautifulSoup(page.content, 'html.parser')
+
+            new_product = {
+                "productname": soup.find(id='productTitle').get_text().replace("\n", ""),
+                "url": url,
+                "tag": soup.find(class_="a-link-normal a-color-tertiary").get_text().replace("  ", "").replace('\n', ''),
+                "detectionprice": []
             }
-        self.product.append(new_product)
+            self.product.append(new_product)
 
-        json.dump(self.product, open(self.settings["productfile"], "w"))
+            json.dump(self.product, open(self.settings["productfile"], "w"))
 
     def new_detection(self, i):
-        page = requests.get(self.product[i]["url"], headers=self.settings["header"])
+        try:
+            page = requests.get(
+                self.product[i]["url"], headers=self.settings["header"])
 
-        if not page.status_code == 200:
-            print("connection error "+page.status_code+" try later")
-            return
+        except requests.exceptions.HTTPError as errh:
+            logging.error("Http Error:", errh)
 
+        except requests.exceptions.ConnectionError as errc:
+            logging.error("Error Connecting:", errc)
 
+        except requests.exceptions.Timeout as errt:
+            logging.error("Timeout Error:", errt)
 
-        price = self.find_product(page)
-        if not price:
-            return
-        # price convertion to string to float
-        price = price.replace(',', '.')
-        price = price[:price.index("€")] #"".join(i for i in price if i != '€')  # price[price.index("€")+1:] by P.
-        converted_price = float(price)
+        except requests.exceptions.RequestException as err:
+            logging.error("OOps: Something Else", err)
 
-        self.product[i]["detectionprice"].append({
-                "price":converted_price,
+        else:
+            '''
+                if not page.status_code == 200:
+                print("connection error "+page.status_code+" try later")
+                logging.error(page.status_code + " - "+ page.url)
+                return
+            '''
+
+            price = self.find_price(page)
+            if not price or price == "":
+                return
+
+            if not re.findall("[0-9]+,[0,9]+", price):
+                price = "0.0€"
+
+            # price convertion to string to float
+            price = price.replace(',', '.')
+            # "".join(i for i in price if i != '€')  # price[price.index("€")+1:] by P.
+            price = re.findall("[0-9]+.[0,9]+", price)[0]
+            converted_price = float(price)
+
+            self.product[i]["detectionprice"].append({
+                "price": converted_price,
                 "date": datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
             })
 
-        json.dump(self.product, open(self.settings["productfile"], "w"))
+            json.dump(self.product, open(self.settings["productfile"], "w"))
 
     def remove_product(self, i):
-        self.product.remove(self.product[i])  
+        self.product.remove(self.product[i])
         json.dump(self.product, open(self.settings["productfile"], "w"))
 
     def show_products(self):
         for x in range(len(self.product)):
             print(f'{x + 1}. {self.product[x]["productname"]}\n')
 
-    def find_product(self, page):
+    def find_price(self, page):
         # 1st try
         soup = BeautifulSoup(page.content, 'html.parser')
-        price = soup.find(id='priceblock_saleprice')
 
-        if not price:
-            # 2nd try
-            price = soup.find(class_='a-price a-text-price a-size-medium apexPriceToPay')
-            if not price:
-                print("nessun prezzo rilevato")
-                print("Si può ipotizzare che il prodotto non è più disponibile")
-            else:
-                print(price)
+        for query in self.settings["serchQuery"]:
+            price = soup.find(class_=query)
+            if not (price == None or price == ""):
+                return price.get_text()
 
-        return price.get_text()
+        logging.warn('prenzo non trovato verra effetuato un report')
+        logging.warn(page.url)
+        return "0.0€"
 
-    def start_continuous_fetch(self, time=1):
-        cont = 1
-        while(True):
-            for i in range(len(self.product)):
-                self.new_detection(i)
-                print("fecting list process number:", cont)
-                cont += 1
-                sleep(time)
+    def start_continuous_fetch(self):
+        logging.info("numero prodotti: "+str(len(self.product)))
+        for i in range(len(self.product)):
+            self.new_detection(i)
+        t = threading.Timer(30, self.start_continuous_fetch)
+        t.start()
 
     def drow_graph(self, i):
-        #todo: fix plot data 
+        # todo: fix plot data
         fig, ax = plt.subplots()
 
         prices = []
@@ -311,7 +343,9 @@ bot = amazonpy()
 
 
 def main():
-    bot.url_menu()
+    # bot.url_menu()
+    bot.start_continuous_fetch()
+
 
 if __name__ == "__main__":
     main()
